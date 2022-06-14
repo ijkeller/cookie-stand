f36a197c6cbfa43103a7b6c632eb564c8ae65c51
Isaiah Keller <isaiah.keller@gmail.com>
2022-06-13 17:13:45 -0700

    changed name of sales.html, created class function and everything is working.

--- app.js
+++ app.js
@@ -1,47 +1,30 @@
 
+function hourlyCustomers(max, min, avg) {
+    let hourlyArray = [];
+    for (i = 0; i <= 12; i++) {
+        hourlyArray.push(Math.round((Math.round(Math.random() * (max - min) + min) * avg)))
+    }
+    return hourlyArray;
+}
 
-
-
-
-function CookieShop(min, max, avg) {
+function CookieShop(name, min, max, avg) {
+    this.name = name;
     this.hourlyMin = min;
     this.hourlyMax = max;
     this.avgCookies = avg;
-    this.customersPerHour = function (min, max) {
-        return Math.round(Math.random() * (max - min) + min)
-    }
-    let cph = this.customersPerHour;
-
-    this.hourly = [
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-        this.customersPerHour,
-    ]
-    let hourly = this.hourly;
-    this.totalCustomers = function (hourly) {
-        return hourly.reduce()
-    }
+    this.hourlyArray = hourlyCustomers(max, min, avg)
 }
 
-const seatle = new CookieShop(23, 65, 6.3);
-const tokyo = new CookieShop(3, 24, 1.2);
-const dubai = new CookieShop(11, 38, 3.7);
-const paris = new CookieShop(20, 38, 2.3);
-const lima = new CookieShop(2, 16, 4.6);
+const seatle = new CookieShop('seatle', 23, 65, 6.3);
+const tokyo = new CookieShop('tokyo', 3, 24, 1.2);
+const dubai = new CookieShop('dubai', 11, 38, 3.7);
+const paris = new CookieShop('paris', 20, 38, 2.3);
+const lima = new CookieShop('lima', 2, 16, 4.6);
 
-console.log(seatle)
-console.log(tokyo)
-console.log(dubai)
-console.log(paris)
-console.log(lima)
+const seatleArray = seatle.hourlyArray
+
+// console.log(seatle)
+// console.log(tokyo)
+// console.log(dubai)
+// console.log(paris)
+// console.log(lima)
